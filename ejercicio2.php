3ee625ac6764c0a313d015cfe2cfa80765c98255
JuanGJ <juangj1b@gmail.com>
2022-01-11 14:12:11 +0100

    Ejercicio 2 - Preparativos

---
+++ ejercicio2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 2 - POO - Subclases</title>
+</head>
+<body>
+<?php
+include "cabecera.php";
+echo "<h2>Ejercicio 2</h2>";
+
+include_once "funciones_ej2.php";
+
+$Persona1 = new Persona("Juan","12345678A","Masculino","85.3","1.75");
+
+?>
+</body>
+</html>
---
+++ funciones_ej2.php
@@ -0,0 +1,87 @@
+<?php
+// Aquí se realizará la definición de la clase y constructor Persona
+class Persona{
+
+    protected $nombre;
+    protected $dni;
+    protected $sexo;
+    protected $peso;
+    protected $altura;
+
+    // Constructor del objeto Persona con sus variables
+    public function __construct($nombre, $dni, $sexo, $peso, $altura)
+    {
+        $this->nombre = $nombre;
+        $this->dni = $dni;
+        $this->sexo = $sexo;
+        $this->peso = $peso;
+        $this->altura = $altura;
+    }
+    // Fin constructor del objeto Persona
+
+    // Get y setter (consultor y modificador) de cada una de las variables
+
+    // Variable nombre
+
+        public function getNombre(){
+            return $this->nombre;
+        }
+
+        public function setNombre($nombre){
+            $this->nombre = $nombre;
+        }
+
+    // Variable DNI
+
+        public function getDNI(){
+            return $this->dni;
+        }
+
+        public function setDNI($dni){
+            $this->dni = $dni;
+        }
+
+    // Variable sexo
+
+        public function getSexo(){
+            return $this->sexo;
+        }
+
+        public function setSexo($sexo){
+            $this->sexo = $sexo;
+        }
+
+    // Variable peso
+
+        public function getPeso(){
+            return $this->peso;
+        }
+
+        public function setPeso($peso){
+            $this->peso = $peso;
+        }
+
+    // Variable altura
+
+        public function getAltura(){
+            return $this->altura;
+        }
+
+        public function setAltura($altura){
+            $this->altura = $altura;
+        }
+
+    // Fin Get y Setters de las variables
+
+    // Funcion mostrar en el que muestre una línea con todos los atributos
+
+    public function Mostrar(){
+        return "$this->nombre, con DNI $this->dni, sexo $this->sexo, $this->peso kg y $this->altura m";
+    }
+
+}
+
+
+
+
+?>
