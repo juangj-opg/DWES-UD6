2cde2fe6b3084f3a7e42000f5e645b9d79ae0f81
JuanGJ <juangj1b@gmail.com>
2022-01-11 12:54:26 +0100

    Preparando la interfaz para los ejercicios

---
+++ cabecera.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cabecera</title>
+</head>
+<!-- Estilos usados -->
+<link rel="stylesheet" href="styles.css">
+<!-- WireFrame usado -->
+<!-- https://wireframe.cc/sqRwM5 -->
+<body>
+
+<!-- Cabecera -->
+
+<div id="cabecerapadre">
+ <div id="cabecerahijo">
+     <a href="./index.php">
+        <h1 id="titulo">DWS</h1>
+        <h2 id="titulo">Ejercicios UD6</h2>
+    </a>
+ </div>
+</div>
+<div id="contenedor">
+
+<!-- Fin Cabecera -->
+
+<!-- Menú -->
+<?php
+        $Ruta = $_SERVER['PHP_SELF'];
+        $Nombre = explode("/",$Ruta);
+        //  echo $Nombre[2]; // Es la última ruta
+?>
+
+<div style="width: 25%; float:left" id="menu">
+    <ul>
+    <li><a <?php if($Nombre[2] == "index.php" ) { echo "class='active'"; } ?> href="./index.php">Inicio</a></li>
+    <li><a <?php if($Nombre[2] == "ejercicio1.php" ) { echo "class='active'"; } ?> href="./ejercicio1.php">Ej. 1 - POO - Constructores</a></li>
+    </ul>
+</div>
+
+
+
+
+<!-- Fin Menú -->
+
+<!-- Contenido -->
+
+<div style="width: 74%; float:right">
+        
---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+</head>
+<body>
+<?php
+include "cabecera.php";
+?>
+<h1 style="text-align: center;">Índice y explicación de los ejercicios del UD5</h1>
+<h2>Ejercicio 1 - POO - Constructores</h2>
+<ul>
+
+</ul>
+
+</body>
+</html>
