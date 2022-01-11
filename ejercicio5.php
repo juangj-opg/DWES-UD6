125b3ac9f0bfcadb013d39b77ba0c630a7cd8d39
JuanGJ <juangj1b@gmail.com>
2022-01-11 23:31:10 +0100

    Ejercicio 5 - Completado

--- ejercicio5.php
+++ ejercicio5.php
@@ -13,6 +13,15 @@ echo "<h2>Ejercicio 5</h2>";
 
 include_once "funciones_ej5.php";
 
+$Equipo = new EquipoFutbol("Sevilla", "38", "UEFA");
+
+echo "<h3>Interfaz IEquipoFutbol</h3>";
+echo "<p>Esta interfaz incluía dos métodos, juegaPartido(), el cual le hemos concatenado 'VICTORIA', y numeroJugadores().</p>";
+echo $Equipo->juegaPartido() . " - ". $Equipo->getNumeroJugadores()." jugadores en toda la plantilla.";
+
+echo "<h3>Interfaz ICampeonato</h3>";
+echo "<p>Esta interfaz añade únicamente un método del cual nombra el campeonato que esté en el equipo (inventado).</p>";
+echo $Equipo->getNombreCampeonato();
 
 ?>
 </body>
