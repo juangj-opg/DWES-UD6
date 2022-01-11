ed73dd4d562eae63d9c831c88ce474dd25c6f8a7
JuanGJ <juangj1b@gmail.com>
2022-01-12 00:05:48 +0100

    Ejercicicio 7 - Preparativos

---
+++ ejercicio7.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 7 - POO - Jerarquia de clase</title>
+</head>
+<body>
+<?php
+include "cabecera.php";
+echo "<h2>Ejercicio 7</h2>";
+
+include_once "funciones_ej7.php";
+
+?>
+</body>
+</html>
