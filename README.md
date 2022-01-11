fd9674ce38531486e033f1f91a6b766fd19e2689
JuanGJ <juangj1b@gmail.com>
2022-01-11 13:05:40 +0100

    Info - Ejercicio 1

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # DWES-UD6
 DWS - Ejercicios del Tema 6 - POO y MVC 
+
+# Ejercicio 1 - POO - Constructores
+ - **ejercicio1.php**:
+   - Desarrollar el código PHP necesario para crear una clase de nombre **Persona** que almacene las siguientes variables:
+     - Nombre
+     - DNI
+     - Sexo
+     - Peso
+     - Altura
+   - Crea un constructor que reciba todos los valores necesario para inicializar el objeto.
+   - Añade los métodos **consultores** y **modificadores** de todos los atributos.
+   - Crea una función **mostrar** que muestre una línea con todos los atributos.
+   - Dentro del archivo, pero fuera de la clase, llama al constructor de la clase y al método mostrar.
+   - Llama a los métodos modificadores y vuelve a invocar al método mostrar.
--- index.php
+++ index.php
@@ -10,10 +10,25 @@
 <?php
 include "cabecera.php";
 ?>
-<h1 style="text-align: center;">Índice y explicación de los ejercicios del UD5</h1>
+<h1 style="text-align: center;">Índice y explicación de los ejercicios del UD6</h1>
 <h2>Ejercicio 1 - POO - Constructores</h2>
 <ul>
-
+    <li><a href="./ejercicio1.php"><h3 class="nopad">ejercicio1.php</h3></a></li>
+    <ul>
+        <li>Desarrollar el código PHP necesario para crear una clase de nombre <b>Persona</b> que almacene las siguientes variables:
+        <ul>
+            <li>Nombre</li>
+            <li>DNI</li>
+            <li>Sexo</li>
+            <li>Peso</li>
+            <li>Altura</li>
+        </ul>   
+        <li>Crea un constructor que reciba todos los valores necesario para inicializar el objeto.</li>
+        <li>Añade los métodos <b>consultores</b> y </b>modificadores</b> de todos los atributos.</li>
+        <li>Crea una función <b>mostrar</b> que muestre una línea con todos los atributos.</li>
+        <li>Dentro del archivo, pero fuera de la clase, llama al constructor de la clase y al método mostrar.</li>
+        <li>Llama a los métodos modificadores y vuelve a invocar al método mostrar.</li>
+    </ul>
 </ul>
 
 </body>
