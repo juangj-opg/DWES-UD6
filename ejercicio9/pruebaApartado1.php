d88aaddb8598c71e1f0b0fddb909fa8a52f4b3f5
JuanGJ <juangj1b@gmail.com>
2022-01-13 12:12:01 +0100

    Ejercicio 9 - A1 - Completado

--- ejercicio9/pruebaApartado1.php
+++ ejercicio9/pruebaApartado1.php
@@ -4,8 +4,11 @@
 *  Desarrolla las clases del esquema dentro de la carpeta includes 
 *  dentro del proyecto. Los métodos se consideran públicos y los atributos privados. 
 *  El resto de ficheros (pruebaApartadoX.php) se deberán ubicar fuera de la carpeta includes.
-*/ 
+*/
+
 spl_autoload_register(function($clase){
     require_once('./includes/class.'.$clase.'.php');
 });
+
+// El apartado 1 solo es crear las clases, con su jerarquía, constructores y funciones.
 ?>
